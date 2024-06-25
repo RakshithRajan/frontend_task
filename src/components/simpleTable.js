0e22db9c8fd0e28059e730a570c21e61ec7294a0
RakshithRajan <123365810+RakshithRajan@users.noreply.github.com>
2024-06-26 03:30:43 +0530

    Update simpleTable.js

--- src/components/simpleTable.js
+++ src/components/simpleTable.js
@@ -1,23 +1,46 @@
 import { React } from "react";
+import "../assets/css/simpleTable.css"
 
-const SimpleTable = ({ dataSource }) => {
+
+import { Table, Button, Popconfirm } from "antd";
+import 'antd/dist/antd.css';
+
+const SimpleTable = ({ dataSource, onDelete }) => {
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <Popconfirm
+          title="Are you sure delete this user?"
+          onConfirm={() => onDelete(record.id)}
+          okText="Yes"
+          cancelText="No"
+        >
+          <Button type="danger">Delete</Button>
+        </Popconfirm>
+      ),
+    },
+  ];
+
   return (
-    <div>
-      {dataSource.length ? (
-        <>
-          {dataSource.map((item, index) => {
-            return (
-              <div key={index}>
-                <div>Id: {item.id}</div>
-                <div>Name: {item.name}</div>
-                <div>Email: {item.email}</div>
-              </div>
-            );
-          })}
-        </>
-      ) : (
-        "No user data"
-      )}
+    <div className="table_output">
+      <Table dataSource={dataSource} columns={columns} />
     </div>
   );
 };
